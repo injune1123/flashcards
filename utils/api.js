cbd41f61c2a51d236afdead638443c3dda9806b1
Yiyi <a11@Yiyis-MBP.fios-router.home>
2021-01-02 22:32:40 -0500

    parse asyncstorage

--- utils/api.js
+++ utils/api.js
@@ -2,5 +2,5 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { DECKS_STORAGE_KEY} from './deck'
 
 export function fetchDecksResults () {
-  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(res=>JSON.parse(res))
 }
