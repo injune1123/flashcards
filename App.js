b444c005ad917b4a77bc63c8592102c2c393d1fd
Yiyi <a11@Yiyis-MBP.fios-router.home>
2021-02-15 23:31:33 -0500

    Add react native router

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
 import { StyleSheet, View } from 'react-native';
-import  AddDeck from './components/AddDeck';
-import  AddCard from './components/AddCard';
+import AddDeck from './components/AddDeck';
+import AddCard from './components/AddCard';
 import DeckOfCards from './components/DeckOfCards';
-import  Decks from './components/Decks';
+import Decks from './components/Decks';
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from './reducers';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// const store = createStore(reducer)
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Decks/>
-      <AddDeck/>
-      <DeckOfCards/>
-      <AddCard/>
-    </View>
+      <StatusBar style="auto"/>
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Home" component={Decks} />
+        <Stack.Screen name="AddDeck" component={AddDeck} />
+        <Stack.Screen name="DeckOfCards" component={DeckOfCards} />
+        <Stack.Screen name="AddCard" component={AddCard} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
