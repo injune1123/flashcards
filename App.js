ad097f3ae81fb65a92d1d71a739c7496c4c50a4b
Yiyi <a11@Yiyis-MBP.fios-router.home>
2021-02-17 00:13:54 -0500

    use tab navigation

--- App.js
+++ App.js
@@ -10,20 +10,49 @@ import { Provider } from "react-redux";
 import reducer from './reducers';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <StatusBar style="auto"/>
       <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen name="Home" component={Decks} />
-        <Stack.Screen name="AddDeck" component={AddDeck} />
-        <Stack.Screen name="DeckOfCards" component={DeckOfCards} />
-        <Stack.Screen name="AddCard" component={AddCard} />
-        </Stack.Navigator>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'AddDeck') {
+              iconName = focused 
+                ? 'ios-albums' 
+                : 'ios-albums';
+            } else if (route.name === 'DeckOfCards') {
+              iconName = focused 
+                ? 'ios-browsers' 
+                : 'ios-browsers';
+            }  else if (route.name === 'AddCard') {
+              iconName = focused 
+                ? 'ios-add' 
+                : 'ios-add';
+            } 
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        >
+          <Tab.Screen name="Home" component={Decks} />
+          <Tab.Screen name="AddDeck" component={AddDeck} />
+          <Tab.Screen name="DeckOfCards" component={DeckOfCards} />
+          <Tab.Screen name="AddCard" component={AddCard} />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
